ef3dc73702ecc08eccecafe6730780cbe29f6406
syduc993 <le.syduc.993@gmail.com>
2025-10-23 17:53:16 +0700

    Initial commit

--- main.py
+++ main.py
@@ -2,4 +2,11 @@ import uvicorn
 from src.core.app import app
 
 if __name__ == "__main__":
-    uvicorn.run("src.core.app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(
+        "src.core.app:app", 
+        host="0.0.0.0", 
+        port=8000, 
+        reload=True,
+        proxy_headers=True,
+        forwarded_allow_ips='*'
+    )
